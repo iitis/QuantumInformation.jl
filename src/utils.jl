d65df43864a65ae4acfb93ab61cab3c15cf7fb7e
Lukasz Pawela <lukasz.pawela@silequipe.com>
2015-04-26 22:07:41 +0200

    Change funcmh to overwrite input

--- src/utils.jl
+++ src/utils.jl
@@ -12,10 +12,40 @@ function renormalize!{T<:Union(Float64, Complex128)}(ρ::Matrix{T})
     end
 end
 
-function funcmh(H::Matrix, f::Function)
-  w,v = eig(Hermitian(H))
-  fw=diagm(Float64[f(x) for x in w])
-  return v*fw*v'
+function funcmh!{T<:Union(Float64, Complex128)}(f::Function, H::Hermitian{T}, R::Matrix{T})
+    F = eigfact!(H)
+    for i=1:length(F[:values])
+        F[:values][i] = f(F[:values][i])
+    end
+    times_diag = zeros(F[:vectors])
+    for i=1:size(F[:vectors], 2)
+        times_diag[:, i] = F[:vectors][:, i] * F[:values][i]
+    end
+    A_mul_Bc!(R, times_diag, F[:vectors])
+end
+
+function funcmh!{T<:Union(Float64, Complex128)}(f::Function, H::Hermitian{T})
+    R = zeros(T, size(H))
+    funcmh!(f, H, R)
+    R
+end
+
+function funcmh{T<:Union(Float64, Complex128)}(f::Function, H::Hermitian{T})
+    R = zeros(T, size(H))
+    funcmh!(f, copy(H), R)
+    R
+end
+
+function funcmh!{T<:Union(Float64, Complex128)}(f::Function, H::Matrix{T}, R::Matrix{T})
+    ishermitian(H) ? funcmh!(f, Hermitian(H), R) : error("Non-hermitian matrix passed to funcmh")
+end
+
+function funcmh!{T<:Union(Float64, Complex128)}(f::Function, H::Matrix{T})
+    ishermitian(H) ? funcmh!(f, Hermitian(H)) : error("Non-hermitian matrix passed to funcmh")
+end
+
+function funcmh{T<:Union(Float64, Complex128)}(f::Function, H::Matrix{T})
+    ishermitian(H) ? funcmh(f, Hermitian(H)) : error("Non-hermitian matrix passed to funcmh")
 end
 
 function random_sphere(dim::Int)
