2dfab2218327914bc2c248894dc0284a5d0af0c2
Łukasz Pawela <lpawela@iitis.pl>
2018-05-14 21:00:18 +0200

    Sparse partial transpose with tests

--- src/base.jl
+++ src/base.jl
@@ -175,6 +175,30 @@ function ptranspose(ρ::AbstractMatrix{T}, idims::Vector, isystems::Vector) wher
     reshape(tensor, size(ρ))
 end
 
+function ptranspose(ρ::SparseMatrixCSC{T, Int64}, dims::Vector, sys::Int) where T<:Number
+
+    if size(ρ,1)!=size(ρ,2)
+        error("Non square matrix passed to ptrace")
+    end
+    if prod(dims)!=size(ρ,1)
+        error("Product of dimensions do not match shape of matrix.")
+    end
+
+    if sys != 1 && sys != 2
+        error("sys must be 1 or 2, not $sys")
+    end
+
+    I, J, V = findnz(ρ)
+    newI = zeros(I)
+    newJ = zeros(J)
+    for k=1:length(I)
+        i, j = number2mixedradix(I[k]-1, dims), number2mixedradix(J[k]-1, dims)
+        i[sys], j[sys] = j[sys], i[sys]
+        newI[k], newJ[k] = mixedradix2number(i, dims), mixedradix2number(j, dims)
+    end
+    transpose(sparse(newI+1, newJ+1, V, size(ρ)...))
+end
+
 function number2mixedradix(n::Int, bases::Vector{Int})
     if n >= prod(bases)
         error("number to big to transform")
