350bc2e51eef1bc694e47bebd9562e781fc2c6cb
Łukasz Pawela <lpawela@iitis.pl>
2018-05-17 17:13:52 +0200

    Rectangularity parameter for random states

--- src/randomstate.jl
+++ src/randomstate.jl
@@ -4,9 +4,8 @@ function random_ket!(ϕ::T) where {T<:AbstractVector{T1}} where {T1<:Real}
 end
 
 function random_ket!(ϕ::T) where {T<:AbstractVector{T1}} where {T1<:Complex}
-    for i=1:length(ϕ)
-        ϕ[i] = randn() + 1im * randn()
-    end
+    n = length(ϕ)
+    ϕ[:] = randn(n) + 1im * randn(n)
     renormalize!(ϕ)
 end
 
@@ -18,31 +17,37 @@ end
 
 random_ket(d::Int) = random_ket(ComplexF64, d)
 
-function random_mixed_state_hs!!(ρ::AbstractMatrix{T}, A::AbstractMatrix{T}) where T<:Real
+function random_mixed_state!!(ρ::AbstractMatrix{T}, A::AbstractMatrix{T}) where T<:Union{Real, Complex}
     random_ginibre_matrix!(A)
-    ρ[:] = A*A'    # A_mul_Bt!(ρ, A, A) deprecated
+    ρ[:] = A*A'
     renormalize!(ρ)
 end
 
-function random_mixed_state_hs!!(ρ::AbstractMatrix{T}, A::AbstractMatrix{T}) where T<:Complex
-    random_ginibre_matrix!(A)
-    ρ[:] = A*A'    # A_mul_Bc!(ρ, A, A) deprecated
+function random_mixed_state!(ρ::AbstractMatrix{T}, c::Real) where T<:Union{Real, Complex}
+    d = size(ρ, 1)
+    A = zeros(T, d, round(Int, c*d))
+    random_mixed_state!!(ρ, A)
     renormalize!(ρ)
 end
 
-function random_mixed_state_hs!(ρ::AbstractMatrix{T}) where T<:Union{Real, Complex}
-    A = zeros(ρ)
-    random_mixed_state_hs!!(ρ, A)
-    renormalize!(ρ)
+function random_mixed_state!(ρ::AbstractMatrix{T}) where T<:Union{Real, Complex}
+    random_mixed_state!(ρ, 1)
 end
 
-function random_mixed_state_hs(::Type{T}, d::Int64) where T<:Union{Real, Complex}
+function random_mixed_state(::Type{T}, d::Int64, c::Real) where T<:Union{Real, Complex}
     ρ = zeros(T, d, d)
-    random_mixed_state_hs!(ρ)
+    random_mixed_state!(ρ, c)
     ρ
 end
 
-random_mixed_state_hs(d::Int64) = random_mixed_state_hs(ComplexF64, d)
+function random_mixed_state(::Type{T}, d::Int64) where T<:Union{Real, Complex}
+    random_mixed_state(T, d, 1)
+end
+
+random_mixed_state(d::Int64, c::Real) = random_mixed_state(ComplexF64, d, c)
+random_mixed_state(d::Int64) = random_mixed_state(ComplexF64, d)
+random_mixed_state_hs(d::Int64) = random_mixed_state(d) #backwards compat
+
 
 function random_jamiolkowski_state!(J::AbstractMatrix{T}) where T<:Union{Real, Complex}
     random_dynamical_matrix!(J)
