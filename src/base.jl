ff5ed127a0ed7aff213782217a46cdbcd4090cd6
Łukasz Pawela <lpawela@iitis.pl>
2018-05-17 15:14:23 +0200

    Sparse reshuffle

--- src/base.jl
+++ src/base.jl
@@ -212,7 +212,7 @@ end
 
 ptranspose(ρ::AbstractMatrix{T}, idims::Vector, sys::Int) where T<:Number = ptranspose(ρ, idims, [sys])
 
-function ptranspose(ρ::SparseMatrixCSC{T, Int64}, dims::Vector, sys::Int) where T<:Number
+function ptranspose(ρ::AbstractSparseMatrix{T}, dims::Vector, sys::Int) where T<:Number
 
     if size(ρ,1)!=size(ρ,2)
         error("Non square matrix passed to ptrace")
@@ -241,7 +241,7 @@ function number2mixedradix(n::Int, bases::Vector{Int})
         error("number to big to transform")
     end
 
-    digits = Array(Int64, length(bases))
+    digits = Array{Int64}(length(bases))
     for (i, base) in enumerate(reverse(bases))
         n, digits[i] = divrem(n, base)
     end
@@ -266,15 +266,44 @@ end
   Given multiindexed matrix M_{(m,μ),(n,ν)} it returns
   matrix M_{(m,n),(μ,ν)}.
 """
-function reshuffle(ρ::AbstractMatrix{T}) where T<:Number
-  (r, c) = size(ρ)
-  sqrtr = isqrt(r)
-  sqrtc = isqrt(c)
-  tensor = reshape(ρ, sqrtr, sqrtr, sqrtc, sqrtc)
+function reshuffle(ρ::AbstractMatrix{T}, dims::Matrix{Int}) where T<:Number
+  tensor = reshape(ρ, dims...)
   perm = [4, 2, 3, 1]
   tensor = permutedims(tensor, perm)
-  (r1, r2, c1, c2) = size(tensor)
+  (r1, r2, c1, r2) = size(tensor)
   return reshape(tensor, r1*r2, c1*c2)
+end
+
+function reshuffle(ρ::AbstractMatrix{T}) where T<:Number
+    (r, c) = size(ρ)
+    sqrtr = isqrt(r)
+    sqrtc = isqrt(c)
+    reshuffle(ρ, [sqrtr sqrtr; sqrtc sqrtc])
+end
+
+function reshuffle(ρ::AbstractSparseMatrix{T}, dims::Matrix{Int}) where T<:Number
+    dimsI =dims[1,:]
+    dimsJ =dims[2,:]
+    newdimsI =[dims[1, 1], dims[2, 1]]
+    newdimsJ =[dims[1, 2], dims[2, 2]]
+println(newdimsI)
+println(newdimsJ)
+    I, J, V = findnz(ρ)
+    newI = zeros(I)
+    newJ = zeros(J)
+    for k=1:length(I)
+        i, j = number2mixedradix(I[k]-1, dimsI), number2mixedradix(J[k]-1, dimsJ)
+        i[1], i[2], j[1], j[2] = j[2], i[2], j[1], i[1] #works?
+        newI[k], newJ[k] = mixedradix2number(i, newdimsI), mixedradix2number(j, newdimsJ)
+    end
+    sparse(newI+1, newJ+1, V, prod(newdimsI), prod(newdimsJ))
+end
+
+function reshuffle(ρ::AbstractSparseMatrix{T}) where T<:Number
+    (r, c) = size(ρ)
+    sqrtr = isqrt(r)
+    sqrtc = isqrt(c)
+    reshuffle(ρ, [sqrtr sqrtr; sqrtc sqrtc])
 end
 
 trace_distance(ρ::AbstractMatrix{T}, σ::AbstractMatrix{T}) where T<:Number = sum(abs.(eigvals(Hermitian(ρ - σ))))
--- test/base.jl
+++ test/base.jl
@@ -144,6 +144,33 @@ end
     X = reshape([1:16;], 4, 4)'
     T = [1 2 5 6; 3 4 7 8; 9 10 13 14; 11 12 15 16]
     @test reshuffle(X) == T
+
+    X = sparse(X)
+    T = sparse(T)
+    @test norm(reshuffle(X) - T, 1) ≈ 0 atol=1e-13
+
+    X = reshape(1:36, 6, 6)'
+    T=[1	2	7	8	13	14;
+    3	4	9	10	15	16;
+    5	6	11	12	17	18;
+    19	20	25	26	31	32;
+    21	22	27	28	33	34;
+    23	24	29	30	35	36]
+
+    T2 = [1	2	3	7	8	9	13	14	15;
+    4	5	6	10	11	12	16	17	18;
+    19	20	21	25	26	27	31	32	33;
+    22	23	24	28	29	30	34	35	36]
+
+    @test reshuffle(X, [2 3; 3 2]) == T
+    @test reshuffle(X, [2 3; 2 3]) == T2
+
+    X = sparse(X)
+    T = sparse(T)
+    T2 = sparse(T2)
+    @test norm(reshuffle(X, [2 3; 3 2]) - T, 1) ≈ 0 atol=1e-13
+    @test norm(reshuffle(X, [2 3; 2 3]) - T2, 1) ≈ 0 atol=1e-13
+
 end
 
 @testset "trace_distance" begin
