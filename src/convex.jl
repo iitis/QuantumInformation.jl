017164ce8e7c28a99635a03299d3cc9917110311
Łukasz Pawela <lukasz.pawela@gmail.com>
2018-06-18 17:42:46 +0200

    Restore docstrings in convex.jl

--- src/convex.jl
+++ src/convex.jl
@@ -1,5 +1,13 @@
 using Convex, SCS
 
+"""
+$(SIGNATURES)
+- `J`: matrix.
+- `d1`: ?.
+- `d2`: ?.
+
+Return [diamond norm](https://arxiv.org/pdf/1004.4110.pdf) of matrix `J`.
+"""
 function diamond_norm(J::AbstractMatrix{<:Number}, d1::Int, d2::Int=d1)
   X = ComplexVariable(d1*d2, d1*d2)
   t = 0.5*inner_product(X, J) + 0.5*inner_product(X', J')
@@ -17,6 +25,15 @@ function diamond_norm(J::AbstractMatrix{<:Number}, d1::Int, d2::Int=d1)
   problem.optval
 end
 
+"""
+$(SIGNATURES)
+- `J1`: matrix.
+- `J2`: matrix.
+- `d1`: ?.
+- `d2`: ?.
+
+Return [diamond distance](https://arxiv.org/pdf/1004.4110.pdf) between matrices `J1` and `J2`.
+"""
 function diamond_distance(J1::AbstractMatrix{<:Number}, J2::AbstractMatrix{<:Number}, d1::Int, d2::Int=d1)
     diamond_norm(J1-J2, d1, d2)
 end
