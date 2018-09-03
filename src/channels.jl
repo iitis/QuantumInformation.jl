b7200f91b9b2ec59a3856807ab10cc86b37092b0
Piotr Gawron <gawron@iitis.pl>
2018-09-03 19:32:27 +0200

    Some rework in channels

--- src/channels.jl
+++ src/channels.jl
@@ -393,7 +393,7 @@ end
 
 function Base.convert(::Type{KrausOperators{T1}}, Φ::IdentityChannel{T2}) where {T1<:AbstractMatrix{N1}, T2<:AbstractMatrix{N2}} where {N1<:Number, N2<:Number}
     N = promote_type(N1, N2)
-    # KrausOperators{T1}(T1[eye(N, Φ.idim)], Φ.idim, Φ.odim)    
+    # KrausOperators{T1}(T1[eye(N, Φ.idim)], Φ.idim, Φ.odim)
     KrausOperators{T1}(T1[Matrix{N}(I, Φ.idim, Φ.idim)], Φ.idim, Φ.odim)
 end
 
@@ -525,18 +525,38 @@ end
 ################################################################################
 
 # TODO : Specialise this function for different quantum ops
-function Base.kron(Φ1::T1, Φ2::T2) where {T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}} where {M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
+function Base.kron(::Type{T}, Φ1::T1, Φ2::T2) where {T<:AbstractQuantumOperation{M}, T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}} where {M<:AbstractMatrix{<:Number}, M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
     ko1 = convert(KrausOperators{M1}, Φ1)
     ko2 = convert(KrausOperators{M2}, Φ2)
-    M = promote_type(M1, M2)
+    # M = promote_type(M1, M2)
     v = M[k1⊗k2 for k1 in ko1.matrices for k2 in ko2.matrices]
-    ko = KrausOperators{M1}(v, Φ1.idim * Φ2.idim, Φ1.odim * Φ2.odim)
-    ko
+    ko = KrausOperators{M}(v, Φ1.idim * Φ2.idim, Φ1.odim * Φ2.odim)
+    convert(T, ko)
 end
 
-function Base.kron(Φ1::UnitaryChannel, Φ2::UnitaryChannel)
-    UnitaryChannel(Φ1.matrix ⊗ Φ2.matrix, Φ1.idim * Φ2.idim, Φ1.odim * Φ2.odim)
+function Base.kron(Φ1::T1, Φ2::T2) where {T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}} where {M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
+    M = promote_type(M1, M2)
+    kron(KrausOperators{M}, Φ1, Φ2)
 end
+
+function Base.kron(::Type{T}, Φ1::UnitaryChannel, Φ2::UnitaryChannel) where {T<:AbstractQuantumOperation{M}} where  {M<:AbstractMatrix{<:Number}}
+    matrix = convert(M, Φ1.matrix ⊗ Φ2.matrix)
+    uc = UnitaryChannel(matrix, Φ1.idim * Φ2.idim, Φ1.odim * Φ2.odim)
+    convert(T, uc)
+end
+
+function Base.kron(Φ1::UnitaryChannel{M1}, Φ2::UnitaryChannel{M2}) where {M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
+    M = promote_type(M1, M2)
+    kron(UnitaryChannel{M}, Φ1, Φ2)
+end
+#
+# function ⊗(Φ1::T1, Φ2::T2) where {T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}} where {M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
+#     M = promote_type(M1, M2)
+#     kron(SuperOperator{M}, ϕ1, ϕ2)
+# end
+#
+# const ⊗ᵪ = kron_chi
+
 
 # # """
 # #     Compose channels in sequence
@@ -552,19 +572,21 @@ end
 #     T(SuperOperator{TM}(s1.matrix * s2.matrix, s1.idim, s2.odim))
 # end
 
-function compose(::Type{T1}, Φ1::T2, Φ2::T3) where {T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}, T3<:AbstractQuantumOperation{M3}} where {M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}, M3<:AbstractMatrix{<:Number}}
+function compose(::Type{T}, Φ1::T1, Φ2::T2) where {T<:AbstractQuantumOperation{M}, T1<:AbstractQuantumOperation{M1}, T2<:AbstractQuantumOperation{M2}} where {M<:AbstractMatrix{<:Number}, M1<:AbstractMatrix{<:Number}, M2<:AbstractMatrix{<:Number}}
     if Φ1.odim != Φ2.idim
         throw(ArgumentError("Channels are incompatible"))
     end
     s1 = convert(SuperOperator{M1}, Φ1)
     s2 = convert(SuperOperator{M2}, Φ2)
-    so = SuperOperator{M3}(M3(s1.matrix * s2.matrix), s1.idim, s2.odim)
-    T3(so)
+    matrix = convert(M, s1.matrix * s2.matrix)
+    so = SuperOperator{M}(matrix, s1.idim, s2.odim)
+    convert(T, so)
 end
 
 function compose(Φ1::T, Φ2::T) where {T<:AbstractQuantumOperation{<:Number}}
     compose(T, Φ1, Φ2)
 end
+
 function Base.:*(Φ1::T, Φ2::T) where {T<:AbstractQuantumOperation{<:Number}}
     compose(Φ1, Φ2)
 end
