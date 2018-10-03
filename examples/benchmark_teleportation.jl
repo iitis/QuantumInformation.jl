d24310751d0762c1f5ba65d8f5339c0130f61152
Łukasz Pawela <lpawela@iitis.pl>
2018-10-03 12:50:50 +0200

    Shoulkd this be here??

--- examples/benchmark_teleportation.jl
+++ examples/benchmark_teleportation.jl
@@ -2,23 +2,24 @@ using QI
 using LinearAlgebra
 using Statistics
 
-steps = 100
-haar = HaarKet(2)
-ψ = (ket(0, 4) + ket(3, 4))/sqrt(2)
-post = [PostSelectionMeasurement(proj(ket(i, 4)) ⊗ 𝕀(2)) for i=0:3]
-rots = [UnitaryChannel(𝕀(2)), UnitaryChannel(sx), UnitaryChannel(sz), UnitaryChannel(sx*sz)]
-had = UnitaryChannel{Matrix{ComplexF64}}(hadamard(2))
-cnot = UnitaryChannel{Matrix{ComplexF64}}([1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0])
-r = zeros(steps, length(γs), 4)
-for (k, γ) in enumerate(γs)
-    for i = 1:steps
-        ϕ = rand(haar)
-        ξ = ϕ ⊗ ψ
-        ρ = ((had ⊗ IdentityChannel(4))∘(cnot ⊗ IdentityChannel(2))∘(IdentityChannel(4) ⊗ Φ(γ)))(ξ)
-        for j=1:4
-            σ = rots[j](ptrace(post[j](ρ), [2, 2, 2], [1, 2]))
-            r[i, k, j] = fidelity(ϕ, σ/tr(σ))
-        end
+
+function teleport(d::Int)
+    haar = HaarKet(2)
+    ψ = reshuffle(𝕀(d))/sqrt(d)
+    ϕ = rand(haar)
+    had = UnitaryChannel{Matrix{ComplexF64}}(hadamard(d))
+    post = [PostSelectionMeasurement(proj(ket(i, d^2)) ⊗ 𝕀(d)) for i=1:d^2]
+    ξ = ϕ ⊗ ψ
+    ρ = ((had ⊗ IdentityChannel(4))∘(cnot ⊗ IdentityChannel(2))∘(IdentityChannel(4) ⊗ Φ(γ)))(ξ)
+    for j=1:d
+        σ = rots[j](ptrace(post[j](ρ), [2, 2, 2], [1, 2]))
+        r[i, k, j] = fidelity(ϕ, σ/tr(σ))
     end
 end
+
+
+
+rots = [UnitaryChannel(𝕀(2)), UnitaryChannel(sx), UnitaryChannel(sz), UnitaryChannel(sx*sz)]
+cnot = UnitaryChannel{Matrix{ComplexF64}}([1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0])
+r = zeros(steps, length(γs), 4)
 mean(r, dims=1)
