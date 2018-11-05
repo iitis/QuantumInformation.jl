9e8c4b1387ab63e4711a59af5bc7e814eee6bf09
Łukasz Pawela <lpawela@iitis.pl>
2018-11-05 10:52:33 +0100

    PRetty plots in results

--- examples/process_results.jl
+++ examples/process_results.jl
@@ -2,6 +2,7 @@ using JLD2
 using FileIO
 using NPZ
 using Plots
+using LaTeXStrings
 ##
 filename_base = "examples/res/100_4,16,64,256_random"
 ##
@@ -16,7 +17,12 @@ for l in labels
     println(l)
     results[l] = data_qutip[l] ./ data_julia[l]
     # p = plot(data_qutip["dims"], results[l], title=l)
-    p=plot(data_qutip["dims"], [data_qutip[l], data_julia[l]], title=l, label=["qutip", "julia"])
+    p=plot(data_qutip["dims"], [data_qutip[l], data_julia[l]], marker=[:o :^],
+    label=["qutip", "julia"], legend=:topleft)
+    xticks!(p, (collect(0:50:250),[latexstring("$(i)") for i in collect(0:50:250)]))
+    xlabel!(p, L"d")
+    ylabel!(p, L"t")
+    savefig("$(filename_base)_$(l).pdf")
     display(p)
 end
 # l = collect(labels)[3]
