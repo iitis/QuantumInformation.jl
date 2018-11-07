e295f7f30dbbf7daecdb92fcd7de98755e96d90f
Łukasz Pawela <lpawela@iitis.pl>
2018-11-07 15:08:37 +0100

    Add COnvex

--- test/convex.jl
+++ test/convex.jl
@@ -1,7 +1,8 @@
+import Random: seed!
 @testset "Convex" begin
 
 @testset "diamond norm" begin
-    srand(42)
+    seed!(42)
     c = ChoiJamiolkowskiMatrices(3, 4)
     Φ = rand(c)
     @test norm_diamond(Φ) ≈ 1 atol=1e-6
@@ -11,9 +12,9 @@ end
     d = 4
     U1 = qft(d)
     U2 = Matrix{ComplexF64}(I, d, d)
-    J1 = cat([1, 2], [proj(U1[:, i]) for i=1:d]...)
-    J2 = cat([1, 2], [proj(U2[:, i]) for i=1:d]...)
-    @test diamond_distance(DynamicalMatrix(J1,d,d), DynamicalMatrix(J2,d,d)) ≈ 2 atol=1e-6
+    J1 = cat([proj(U1[:, i]) for i=1:d]..., dims=[1, 2])
+    J2 = cat([proj(U2[:, i]) for i=1:d]..., dims=[1, 2])
+    @test diamond_distance(DynamicalMatrix(J1, d, d), DynamicalMatrix(J2, d, d)) ≈ 2 atol=1e-5
 end
 
 end
