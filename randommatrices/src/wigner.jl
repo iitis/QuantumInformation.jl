e52d5aae837345016d77f7e7a8cf3e58b6a2a0c9
Łukasz Pawela <lukasz.pawela@gmail.com>
2019-08-22 18:52:27 +0200

    fix wigner ensemble

--- randommatrices/src/wigner.jl
+++ randommatrices/src/wigner.jl
@@ -2,16 +2,18 @@ export WignerEnsemble
 
 struct WignerEnsemble{β} <: QIContinuousMatrixDistribution
     d::Int
+    g::GinibreEnsemble{β}
 
     function WignerEnsemble{β}(d::Int) where β
         β == 4 && mod(d, 2) == 1 ? throw(ArgumentError("Dim must even")) : ()
-        new(d)
+        g = GinibreEnsemble{β}(d)
+        new(d, g)
     end
 end
 
 WignerEnsemble(d::Int) = WignerEnsemble{2}(d)
 
 function rand(rng::AbstractRNG, w::WignerEnsemble{β}) where β
-    z = rand(rng, GinibreEnsemble{β}(w.d))
+    z = rand(rng, w.g)
     (z + z') / 2sqrt(2β * w.d)
 end
