7816cbe3857bb9a1f1709d092c38bfec1dd4d30e
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-01-24 00:57:03 +0100

    partial transpose in tensor cast

--- src/ptranspose.jl
+++ src/ptranspose.jl
@@ -7,31 +7,31 @@ $(SIGNATURES)
 
 Return [partial transposition](http://en.wikipedia.org/wiki/Peres-Horodecki_criterion) of matrix `ρ` over the subsystems determined by `isystems`.
 """
-# function ptranspose(ρ::AbstractMatrix{<:Number}, idims::Vector{Int}, isystems::Vector{Int})
-#     dims = reverse(idims)
-#     systems = length(idims) .- isystems .+ 1
+function _ptranspose(ρ::AbstractMatrix{<:Number}, idims::Vector{Int}, isystems::Vector{Int})
+    dims = reverse(idims)
+    systems = length(idims) .- isystems .+ 1
 
-#     if size(ρ,1)!=size(ρ,2)
-#         throw(ArgumentError("Non square matrix passed to ptrace"))
-#     end
-#     if prod(dims)!=size(ρ,1)
-#         throw(ArgumentError("Product of dimensions do not match shape of matrix."))
-#     end
-#     if maximum(systems) > length(dims) ||  minimum(systems) < 1
-#         throw(ArgumentError("System index out of range"))
-#     end
+    if size(ρ,1)!=size(ρ,2)
+        throw(ArgumentError("Non square matrix passed to ptrace"))
+    end
+    if prod(dims)!=size(ρ,1)
+        throw(ArgumentError("Product of dimensions do not match shape of matrix."))
+    end
+    if maximum(systems) > length(dims) ||  minimum(systems) < 1
+        throw(ArgumentError("System index out of range"))
+    end
 
-#     offset = length(dims)
-#     tensor = reshape(ρ, [dims; dims]...)
-#     perm = collect(1:(2offset))
-#     for s in systems
-#         idx1 = findall(x->x==s, perm)[1]
-#         idx2 = findall(x->x==(s + offset), perm)[1]
-#         perm[idx1], perm[idx2] = perm[idx2], perm[idx1]
-#     end
-#     tensor = permutedims(tensor, invperm(perm))
-#     reshape(tensor, size(ρ))
-# end
+    offset = length(dims)
+    tensor = reshape(ρ, [dims; dims]...)
+    perm = collect(1:(2offset))
+    for s in systems
+        idx1 = findall(x->x==s, perm)[1]
+        idx2 = findall(x->x==(s + offset), perm)[1]
+        perm[idx1], perm[idx2] = perm[idx2], perm[idx1]
+    end
+    tensor = permutedims(tensor, invperm(perm))
+    reshape(tensor, size(ρ))
+end
 
 """
 $(SIGNATURES)
@@ -44,18 +44,26 @@ ptranspose(ρ::AbstractMatrix{<:Number}, idims::Vector{Int}, sys::Int) = ptransp
 function ptranspose(ρ::AbstractMatrix{<:Number}, idims::Vector{Int}, isystems::Vector{Int})
     ns = length(idims)
 
-    ex = Expr(:(:=))
-    ex1 = Expr(:ref)
-    push!(ex1.args, :x)
+    ex1 = Expr(:ref, :x)
+    ex2 = Expr(:ref, ρ)
 
-    ex2 = Expr(:ref)
-    push!(ex2.args, :ρ)
+    I = Expr(:tuple, [gensym() for _=1:ns]...)
+    J = Expr(:tuple, [gensym() for _=1:ns]...)
 
-    I = Tuple(gensym() for _=1:ns)
-    J = Tuple(gensym() for _=1:ns)
+    K = copy(I)
+    L = copy(J)
+
+    r = Expr(:tuple)
+    for (k, (i, j)) in enumerate(zip(K.args, L.args))
+        push!(r.args, :($i:$(idims[k])), :($j:$(idims[k])))
+    end
+    for s in isystems
+        K.args[s], L.args[s] = L.args[s], K.args[s]
+    end
     push!(ex1.args, I, J)
-    push!(ex2.args, I, J)
+    push!(ex2.args, L, K)
 
-    ex.args = [ex1, ex2]
-    @eval @cast $ex
+    ex = Expr(:(:=), ex1, ex2)
+    ex, r
+    @eval @cast $ex $r
 end
